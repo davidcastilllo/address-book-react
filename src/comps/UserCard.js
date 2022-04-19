125877bfe6fcb934f47ce5bf04218879250ff20a
DavidCastillo <seth.castillo4@outlook.com>
2022-04-19 00:54:49 -0500

    made adjustment

--- src/comps/UserCard.js
+++ src/comps/UserCard.js
@@ -3,12 +3,17 @@ import '../comps/UserCard.css'
 
 let UserCard = (props) => {
 
-  let toggleInfo = (id) => {
+  let toggleInfo = (id, e) => {
     let infoCard = document.getElementById(id)
     if (infoCard.style.visibility == 'hidden') {
       infoCard.style.visibility = 'visible'
     }
     else {infoCard.style.visibility = 'hidden'}
+
+    if (e.innerText == 'Show Details') {
+      e.innerText = 'Hide Details'
+    }
+    else {e.innerText = 'Show Details'}
   }
 
   return (
@@ -17,7 +22,7 @@ let UserCard = (props) => {
         <section id='card'>
           <div id="name">{props.user.name.first} {props.user.name.last}</div>
           <img src={props.user.picture.medium} id="img"></img>
-          <button id="btn" onClick={() => {toggleInfo(props.id)}}>Toggle Info</button>
+          <button id="btn" onClick={(e) => {toggleInfo(props.id, e.target)}}>Show Details</button>
         </section>
         <section id={props.id} className="info">
           <div> <b>Cell:</b> <br></br>{props.user.cell} </div>
